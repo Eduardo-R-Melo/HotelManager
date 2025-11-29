c550282801948d2fddb19199176aef6c78319c20
Eduardo-R-Melo <reisdemeloeduardo@gmail.com>
2025-11-29 11:22:28 -0400

    dir src/hotel | listas populadas com dados

---
+++ src/hotel.py
@@ -0,0 +1,10 @@
+
+
+def listas_hotel():
+    numeros_quartos = [101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150]
+
+    status_quartos = ["livre", "livre", "limpeza", "manutencao", "manutencao", "limpeza", "limpeza", "ocupado", "limpeza", "ocupado", "livre", "limpeza", "limpeza", "limpeza", "manutencao", "livre", "livre", "manutencao", "manutencao", "ocupado", "manutencao", "ocupado", "manutencao", "limpeza", "ocupado", "ocupado", "manutencao", "livre", "manutencao", "livre", "limpeza", "limpeza", "limpeza", "limpeza", "manutencao", "limpeza", "manutencao", "ocupado", "manutencao", "ocupado", "ocupado", "manutencao", "ocupado", "limpeza", "ocupado", "limpeza", "manutencao", "manutencao", "ocupado", "livre"]
+
+    hospedar_quartos = ["", "", "", "", "", "", "", "Maria Eduarda", "", "João Miguel", "", "", "", "", "", "", "", "", "", "Ana Clara", "", "Pedro Henrique", "", "", "Laura Sofia", "Luiz Otávio", "", "", "", "", "", "", "", "", "", "", "", "Isabela Cristina", "", "Carlos Eduardo", "Vitória Régia", "", "Alice Beatriz", "", "Gabriel Lima", "", "", "", "Bento Augusto", ""]
+
+    dias_estadia = [0, 0, 0, 0, 0, 0, 0, 4, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 10, 0, 0, 17, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 0, 8, 2, 0, 5, 0, 6, 0, 0, 0, 15, 0]
