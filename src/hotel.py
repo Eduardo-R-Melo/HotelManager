2238e702e9b23c08793450c7f0ae80234917049c
Eduardo Melo <reisdemeloeduardo@gmail.com>
2025-12-05 09:41:11 -0400

    hotel.pt | feat: add fuction `realocar_hospede`

--- src/hotel.py
+++ src/hotel.py
@@ -1,13 +1,13 @@
 def inicializar_hotel():
-    numero_quartos = list(range(101, 151))
+    numeros_quartos = list(range(101, 151))
 
-    status_quarto = ["livre"] * 50
+    status_quartos = ["livre"] * 50
 
     hospedes_quartos = [""] * 50
 
     dias_estadia = [0] * 50
 
-    return numero_quartos, status_quarto, hospedes_quartos, dias_estadia
+    return numeros_quartos, status_quartos, hospedes_quartos, dias_estadia
 
 def encontrar_indice_quarto(num_quarto):
     
@@ -67,8 +67,8 @@ def marcar_quarto_limpo(num_quarto):
 
     indice = encontrar_indice_quarto(num_quarto)
 
-    if status[indice] != "limpeza" and status[indice] != "manuntencao":
-        print('Quarto não esta em (Limpeza/Manuntenção)')
+    if status[indice] != "limpeza" and status[indice] != "manutencao":
+        print('Quarto não esta em (Limpeza/Manutenção)')
 
         return False
 
@@ -94,6 +94,34 @@ def encontra_quartos_por_status(status_busca):
     
     print(f'Quartos encontrados: {quartos_encontrados}')
 
+def realocar_hospede(quarto_origem, quarto_destino):
+    indice_origem = encontrar_indice_quarto(quarto_origem)
+    indice_destino = encontrar_indice_quarto(quarto_destino)
+
+    if indice_origem == -1 or indice_destino == -1:
+        print('\nNão foi possível realocar os hospedes, verifique os quartos novamente!')
+    
+    elif status[indice_origem] != "ocupado":
+        print('\nAVISO: ORIGEM')
+        print(f'Quarto de origem: {quarto_origem}, não está ocupado!')
+
+    elif status[indice_destino] != "livre":
+        print('\nAVISO: DESTINO')
+        print(f'Quarto de destino: {quarto_destino}, está ocupado!')
+
+    elif numeros[indice_origem] == numeros[indice_destino]:
+        print('\nOperação inválida')
+
+    else:
+        hospedes[indice_destino] = hospedes[indice_origem]
+        dias[indice_destino] = dias[indice_origem]
+        status[indice_destino] = "ocupado"
+        print(f'Hospede: {hospedes[indice_origem]} foi alocado ao quarto: {numeros[indice_destino]}.')
+
+        status[indice_origem] = "limpeza"
+        hospedes[indice_origem] = ""
+        dias[indice_origem] = 0
+        
 escolha = 999
 numeros, status, hospedes, dias = inicializar_hotel()
 print('Bem vindo(a) ao HotelPy!')
@@ -108,6 +136,7 @@ while escolha != 0:
     print('4 - Concluir limpeza')
     print('5 - Visualizar informações dos quartos')
     print('6 - Busca por status')
+    print('7 - Realocar hospede')
 
     escolha = int(input('\nO que deseja visualizar? '))
 
@@ -134,7 +163,6 @@ while escolha != 0:
         fazer_check_in(num_quarto)
 
     if escolha == 3:
-        
         num_quarto = int(input('\nDigite o número do quarto que deseja realizar o check-out: '))
 
         checkOut = fazer_check_out(num_quarto)
@@ -151,3 +179,9 @@ while escolha != 0:
         status_busca = input('Digite qual status deseja buscar:\nlivre\nocupado\nlimpeza\nmanuntencao\n\n')
         
         busca = encontra_quartos_por_status(status_busca)
+
+    if escolha == 7:
+        quarto_origem = int(input('\nDigite o número do quarto de origem do hospede: '))
+        quarto_destino = int(input('\nDigite o número do quarto de destino do hospede: '))
+
+        realocar = realocar_hospede(quarto_origem, quarto_destino)
